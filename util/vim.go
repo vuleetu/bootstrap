8fa9cf4cde478684e86b6fd05ec46b91686e3898
Fisher <fisher@yun.io>
2012-09-01 21:07:50 +0800

    update

--- util/vim.go
+++ util/vim.go
@@ -14,21 +14,16 @@ func (vim *VimPkg) Install(){
     _, e := sexec("which", []string{"zsh"})
     if e != nil {
         log.Print("vim not found, installing vim...")
-        cmd, args := "sudo", []string{"apt-get", "install", "vim"}
-        lexec(cmd, args)
+        config("sudo", []string{"apt-get", "install", "vim"})
     } else {
         log.Print("vim already exists")
     }
     log.Print("backup original .vimrc file")
-    cmd2, args2 := "mv", []string{"~/.vimrc", "~/.vimrc.orig"}
-    lexec(cmd2, args2)
+    config("mv", []string{"~/.vimrc", "~/.vimrc.orig"})
     log.Print("downloading .vimrc")
-    cmd1, args1 := "git",  []string{"clone", "https://github.com/vuleetu/vimrc.git", "~/vimrc/"}
-    lexec(cmd1, args1)
+    config("git",  []string{"clone", "https://github.com/vuleetu/vimrc.git", "~/vimrc/"})
     log.Print("link new .vimrc file")
-    cmd3, args3 := "ln", []string{"-s", "~/vimrc/.vimrc", "~/.vimrc"}
-    lexec(cmd3, args3)
+    config("ln", []string{"-s", "~/vimrc/.vimrc", "~/.vimrc"})
     log.Print("install vim plugin")
-    cmd4, args4 := "vim", []string{"+BundleInstall", "+qa"}
-    lexec(cmd4, args4)
+    config("vim", []string{"+BundleInstall", "+qa"})
 }
