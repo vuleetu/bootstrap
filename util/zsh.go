f0bfb584237f1d8870ade3720d706ac203097adc
Fisher <fisher@yun.io>
2012-09-01 21:06:36 +0800

    update

--- util/zsh.go
+++ util/zsh.go
@@ -1,5 +1,4 @@
 package util
-
 import (
     "log"
 )
@@ -11,22 +10,17 @@ func (zsh *ZshPkg) Install() {
     _, e := sexec("which", []string{"zsh"})
     if e != nil {
         log.Print("zsh not found, installing zsh...")
-        cmd, args := "sudo", []string{"apt-get", "install zsh"}
-        lexec(cmd, args)
+        config("sudo", []string{"apt-get", "install zsh"})
         log.Print("zsh install success")
     } else {
         log.Print("zsh already exists")
     }
     log.Print("download oh-my-zsh")
-    cmd1, args1 := "git",  []string{"clone", "https://github.com/robbyrussell/oh-my-zsh.git", "~/.oh-my-zsh/"}
-    lexec(cmd1, args1)
-    log.Print("backuo original .zshrc file")
-    cmd2, args2 := "cp", []string{"~/.zshrc", "~/.zshrc.orig"}
-    lexec(cmd2, args2)
+    config("git",  []string{"clone", "https://github.com/robbyrussell/oh-my-zsh.git", "~/.oh-my-zsh/"})
+    log.Print("backup original .zshrc file")
+    config("cp", []string{"~/.zshrc", "~/.zshrc.orig"})
     log.Print("create new .zshrc file")
-    cmd3, args3 := "cp", []string{"~/.oh-my-zsh/templates/zshrc.zsh-template", "~/.zshrc" }
-    lexec(cmd3, args3)
+    config("cp", []string{"~/.oh-my-zsh/templates/zshrc.zsh-template", "~/.zshrc" })
     log.Print("change default shell to zsh")
-    cmd4, args4 := "chsh", []string{"-s", "/bin/zsh" }
-    lexec(cmd4, args4)
+    config("chsh", []string{"-s", "/bin/zsh" })
 }
